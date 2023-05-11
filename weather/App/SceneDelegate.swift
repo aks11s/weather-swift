72fe072945c6c3c52428279268cc39b818d27e4f
aks11s <aksis2003@mail.ru>
2023-05-11 23:36:17 +0300

    strong reference в SceneDelegate

--- weather/App/SceneDelegate.swift
+++ weather/App/SceneDelegate.swift
@@ -5,19 +5,21 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var coordinator: MainCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
+
         guard let windowScene = (scene as? UIWindowScene) else { return }
-            
+
         let coordinator = MainCoordinator()
+        self.coordinator = coordinator
+
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = coordinator.navigationController
         self.window = window
         window.makeKeyAndVisible()
         coordinator.start()
-        
+
     }
 
 
