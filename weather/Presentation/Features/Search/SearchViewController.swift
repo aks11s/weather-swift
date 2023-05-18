8b6f4667947d536d0b6a02b320bddd0ab0e06ece
aks11s <aksis2003@mail.ru>
2023-05-18 08:47:33 +0300

    кран поиска города с Geocoding API

---
+++ weather/Presentation/Features/Search/SearchViewController.swift
@@ -0,0 +1,139 @@
+import UIKit
+import SnapKit
+
+class SearchViewController: UIViewController, Routing {
+    weak var coordinator: Coordinator?
+
+    var onLocationSelected: ((Location) -> Void)?
+
+    private let viewModel: SearchViewModel
+
+    // MARK: - UI
+
+    private let searchBar: UISearchBar = {
+        let sb = UISearchBar()
+        sb.placeholder = "Search city..."
+        sb.searchBarStyle = .minimal
+        sb.tintColor = .white
+        sb.barTintColor = .clear
+        sb.searchTextField.textColor = .white
+        sb.searchTextField.backgroundColor = UIColor.white.withAlphaComponent(0.15)
+        sb.searchTextField.leftView?.tintColor = UIColor.white.withAlphaComponent(0.6)
+        return sb
+    }()
+
+    private let tableView: UITableView = {
+        let tv = UITableView()
+        tv.backgroundColor = .clear
+        tv.separatorColor = UIColor.white.withAlphaComponent(0.15)
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tv
+    }()
+
+    private let activityIndicator: UIActivityIndicatorView = {
+        let ai = UIActivityIndicatorView(style: .medium)
+        ai.color = .white
+        ai.hidesWhenStopped = true
+        return ai
+    }()
+
+    // MARK: - Init
+
+    init(viewModel: SearchViewModel = SearchViewModel()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupLayout()
+        setupBindings()
+        searchBar.becomeFirstResponder()
+    }
+
+    // MARK: - Setup
+
+    private func setupViews() {
+        view.backgroundColor = UIColor(red: 38/255, green: 33/255, blue: 113/255, alpha: 0.97)
+
+        view.addSubview(searchBar)
+        view.addSubview(tableView)
+        view.addSubview(activityIndicator)
+
+        searchBar.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    private func setupLayout() {
+        searchBar.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.left.right.equalToSuperview().inset(8)
+        }
+
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom).offset(8)
+            make.left.right.bottom.equalToSuperview()
+        }
+
+        activityIndicator.snp.makeConstraints { make in
+            make.center.equalTo(tableView)
+        }
+    }
+
+    private func setupBindings() {
+        viewModel.resultsDidChange = { [weak self] _ in
+            self?.tableView.reloadData()
+        }
+        viewModel.isLoadingDidChange = { [weak self] loading in
+            loading ? self?.activityIndicator.startAnimating()
+                    : self?.activityIndicator.stopAnimating()
+        }
+    }
+}
+
+// MARK: - UISearchBarDelegate
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        viewModel.search(query: searchText)
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        dismiss(animated: true)
+    }
+}
+
+// MARK: - UITableView
+
+extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.results.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let location = viewModel.results[indexPath.row]
+
+        cell.backgroundColor = .clear
+        cell.textLabel?.textColor = .white
+        cell.textLabel?.text = [location.name, location.region, location.country]
+            .compactMap { $0 }
+            .joined(separator: ", ")
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let location = viewModel.results[indexPath.row]
+        onLocationSelected?(location)
+    }
+}
---
+++ weather/Presentation/Features/Search/SearchViewModel.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+class SearchViewModel {
+    var resultsDidChange: (([Location]) -> Void)?
+    var isLoadingDidChange: ((Bool) -> Void)?
+
+    private(set) var results: [Location] = [] {
+        didSet { resultsDidChange?(results) }
+    }
+
+    private var isLoading = false {
+        didSet { isLoadingDidChange?(isLoading) }
+    }
+
+    private let repository: WeatherRepositoryProtocol
+    private var searchTask: Task<Void, Never>?
+
+    init(repository: WeatherRepositoryProtocol = WeatherRepository()) {
+        self.repository = repository
+    }
+
+    func search(query: String) {
+        searchTask?.cancel()
+        guard !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            results = []
+            return
+        }
+
+        searchTask = Task { @MainActor in
+            // Debounce 400ms
+            try? await Task.sleep(nanoseconds: 400_000_000)
+            guard !Task.isCancelled else { return }
+
+            isLoading = true
+            do {
+                let locations = try await repository.searchLocations(query: query)
+                results = locations
+            } catch {
+                results = []
+            }
+            isLoading = false
+        }
+    }
+}
